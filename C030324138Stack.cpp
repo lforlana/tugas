42903cec0bb2dffeea8b1389c731eff103c1df1d
Lforlana <mhmmdlanaaaa@gmail.com>
2025-06-01 19:49:49 -0700

    Operasi penuh

--- C030324138Stack.cpp
+++ C030324138Stack.cpp
@@ -12,3 +12,14 @@ typedef struct {
 void InitializeStack(Stack *S){
     S->count = 0;
 }
+void awal(Stack *S) {
+    S->Count = 0;
+}
+
+int penuh(Stack *S){
+    return (S->Count == MAXTUMPUKAN);
+}
+
+int kosong(Stack *S){
+    return (S->Count == 0);
+}
